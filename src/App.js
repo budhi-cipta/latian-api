f87b50864885c135f25051bf4b3f5aed02069fba
budhi arta <budhiarta@budhis-MacBook-Pro.local>
2020-08-05 17:05:17 +0800

    latian-api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect,useState } from 'react';
+import axios from 'axios'
+import './App.css'
 
-function App() {
+const App = () => {
+    const [gipNews, setNews] = useState ([]);
+    // const [news, setNews] = useState ([])
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState();
+
+
+  useEffect(() => {
+    //ge api dipisahkan dengan ? trus query isi dengan api key
+     const url = 'https://api.giphy.com/v1/gifs/search?q=spiderman&api_key=eSt3p8OHcGg9Cp4YKGIdLoojhIpUD4s3&limit=9';
+    // const url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&eSt3p8OHcGg9Cp4YKGIdLoojhIpUD4s3&limit=9";
+    
+    axios.get(url)
+    .then(function(result) {
+       console.log(result.data.data)
+      // console.log(result.data.articles)
+      setNews(result.data.data);
+      setLoading(false)
+    })
+    .catch(function(error) {
+      setError (true);
+      console.log(error.Message)
+      setErrorMessage(error.Message)
+      setLoading(false)
+    });
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Call API</h2>
+      <div className="container">
+      {loading ? (
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+      ) : (
+        error ? (
+        <div>{errorMessage}</div>
+        ) : (
+          gipNews.map((item) => (
+            
+            <ul className="foto">
+              <li><img src={item.images.original.url} alt="giphy" /></li>
+              <li>{item.title}</li>
+              <li>{item.type}</li>
+             
+            </ul>
+          ))
+        )
+      )
+      } 
+    </div>   
     </div>
   );
-}
+};
 
 export default App;
